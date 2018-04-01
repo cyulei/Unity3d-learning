22a3b80671087506d8aed0350f35b52a404798f9
cyulei <1678240316@qq.com>
2018-04-01 19:30:35 +0800

    Create Move.cs

---
+++ HW2-SolarSystem/Move.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public Transform father;                //父节点的Transform
+    public float speed = 15;                //速度
+    float ry, rx;                           //rotation x和rotation y
+    // Use this for initialization
+    void Start () {
+        rx = Random.Range(10, 300);         //按照多少度的角度旋转
+        ry = Random.Range(10, 300);
+    }	
+	// Update is called once per frame
+	void Update () {
+        Vector3 axis = new Vector3(rx, ry, 0);
+        this.transform.RotateAround(father.position, axis, speed * Time.deltaTime);
+    }
+}
