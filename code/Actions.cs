6e6f5326e7105a85c4d77e0ff46e6aa769a041a1
cyulei <1678240316@qq.com>
2018-04-08 17:25:02 +0800

    Update Actions.cs

--- code/Actions.cs
+++ code/Actions.cs
@@ -179,27 +179,27 @@ namespace mygame
 	public class MySceneActionManager:SSActionManager  //本游戏管理器
 	{                  
 
-        private SSMoveToAction moveBoatToEndOrStart;     //移动船到结束岸，移动船到开始岸
-        private SequenceAction moveRoleToLandorBoat;     //移动角色到陆地，移动角色到船上
+		private SSMoveToAction moveBoatToEndOrStart;     //移动船到结束岸，移动船到开始岸
+		private SequenceAction moveRoleToLandorBoat;     //移动角色到陆地，移动角色到船上
 
-        public Controllor sceneController;
+		public Controllor sceneController;
 
-        protected new void Start()
-        {
-            sceneController = (Controllor)SSDirector.GetInstance().CurrentScenceController;
-            sceneController.actionManager = this;
-        }
-        public void moveBoat(GameObject boat, Vector3 target, float speed)
-        {
+		protected new void Start()
+		{
+		    sceneController = (Controllor)SSDirector.GetInstance().CurrentScenceController;
+		    sceneController.actionManager = this;
+		}
+		public void moveBoat(GameObject boat, Vector3 target, float speed)
+		{
 		    moveBoatToEndOrStart = SSMoveToAction.GetSSAction(target, speed);
-			this.RunAction(boat, moveBoatToEndOrStart, this);
+		    this.RunAction(boat, moveBoatToEndOrStart, this);
 		}
 
 		public void moveRole(GameObject role, Vector3 middle_pos, Vector3 end_pos,float speed)
-        {
+		{
 			SSAction action1 = SSMoveToAction.GetSSAction(middle_pos, speed);
 			SSAction action2 = SSMoveToAction.GetSSAction(end_pos, speed);
-		    moveRoleToLandorBoat = SequenceAction.GetSSAcition(1, 0, new List<SSAction>{action1, action2});
+			moveRoleToLandorBoat = SequenceAction.GetSSAcition(1, 0, new List<SSAction>{action1, action2});
 			this.RunAction(role, moveRoleToLandorBoat, this);
 		}
 	}		
